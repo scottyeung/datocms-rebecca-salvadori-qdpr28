9a43fd33fdb8875c1aac7991ab2ad14ec57c1264
Scott Yeung <micold@gmail.com>
2021-03-07 00:29:29 +0800

    Mobile styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,42 +6,47 @@ import JobsGrid from '../components/JobsGrid'
 import 'twin.macro'
 
 import {
-  isMobile
+  isMobile, BrowserView, MobileOnlyView
 } from "react-device-detect";
 
 const IndexPage = ({ data: {about, features, screenings, shorts, docs, mv, quali } }) => {
   
-  return !isMobile ? (
-  <Layout>
-    <HelmetDatoCms seo={about.seoMetaTags} />
-    <div tw="grid grid-cols-2 bg-red-500">
-      <div tw="h-screen overflow-scroll bg-white pt-10 px-2">
-      <div tw="py-10">
-        email: <a href={`mailto:${about.eMail}`}>{about.eMail}</a> 
-      </div>
-      <JobsGrid jobs={features} title='Features / Publications' noYear />
-      <JobsGrid jobs={screenings} title='Screenings / Installations / Exhibitions' />
-      <JobsGrid jobs={shorts} title={"Selected Short Documentaries\nDirector | Editor"} />
-      <JobsGrid jobs={docs} title={"Selected Documentary\nDirector, Editor"} />
-      <JobsGrid jobs={mv} title={"Selected Music Videos\nDirector, Editor"} />
-      <JobsGrid jobs={quali} title={"Academic Qualifications"} />
-      </div>
-      <div>
-      </div>
-    </div>
-  </Layout>
-  ): (
-    <Layout tw="overflow-scroll">
-      <div tw="py-10">
-        <a href={`mailto:${about.eMail}`}>{about.eMail}</a>
-        <JobsGrid jobs={features} title='Features / Publications' noYear />
-        <JobsGrid jobs={screenings} title='Screenings / Installations / Exhibitions' />
-        <JobsGrid jobs={shorts} title={"Selected Short Documentaries\nDirector | Editor"} />
-        <JobsGrid jobs={docs} title={"Selected Documentary\nDirector, Editor"} />
-        <JobsGrid jobs={mv} title={"Selected Music Videos\nDirector, Editor"} />
-        <JobsGrid jobs={quali} title={"Academic Qualifications"} />
-      </div>
-    </Layout>
+  return (
+  <>
+    <BrowserView>
+      <Layout>
+        <HelmetDatoCms seo={about.seoMetaTags} />
+        <div tw="grid grid-cols-2 bg-red-500">
+          <div tw="h-screen overflow-scroll bg-white pt-10 px-2">
+          <div tw="py-10">
+            email: <a href={`mailto:${about.eMail}`}>{about.eMail}</a> 
+          </div>
+          <JobsGrid jobs={features} title='Features / Publications' noYear />
+          <JobsGrid jobs={screenings} title='Screenings / Installations / Exhibitions' />
+          <JobsGrid jobs={shorts} title={"Selected Short Documentaries\nDirector | Editor"} />
+          <JobsGrid jobs={docs} title={"Selected Documentary\nDirector, Editor"} />
+          <JobsGrid jobs={mv} title={"Selected Music Videos\nDirector, Editor"} />
+          <JobsGrid jobs={quali} title={"Academic Qualifications"} />
+          </div>
+          <div>
+          </div>
+        </div>
+      </Layout>
+    </BrowserView>
+    <MobileOnlyView>
+      <Layout tw="overflow-scroll">
+        <div tw="py-10">
+          <a href={`mailto:${about.eMail}`}>{about.eMail}</a>
+          <JobsGrid jobs={features} title='Features / Publications' noYear />
+          <JobsGrid jobs={screenings} title='Screenings / Installations / Exhibitions' />
+          <JobsGrid jobs={shorts} title={"Selected Short Documentaries\nDirector | Editor"} />
+          <JobsGrid jobs={docs} title={"Selected Documentary\nDirector, Editor"} />
+          <JobsGrid jobs={mv} title={"Selected Music Videos\nDirector, Editor"} />
+          <JobsGrid jobs={quali} title={"Academic Qualifications"} />
+        </div>
+      </Layout>
+    </MobileOnlyView>
+    </>
   )
 }
 
